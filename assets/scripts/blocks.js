7cb835784c67d6a36839b940267b009ff7eb4b1c
Omkar-Chaskar <Omkarchaskar1402@gmail.com>
2021-09-02 02:15:36 +0530

    update : bot block functionality

--- assets/scripts/blocks.js
+++ assets/scripts/blocks.js
@@ -9,10 +9,11 @@ $(document).ready(function () {
 
 //Create a Block with Name Bot 
 Blockly.Blocks['example_bot'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField("Bot");
-    this.setNextStatement(true, null);
+      .appendField("Bot");
+    this.appendStatementInput("question")
+      .setCheck(null);
     this.setColour(230);
     this.setTooltip("");
     this.setHelpUrl("");
@@ -21,17 +22,16 @@ Blockly.Blocks['example_bot'] = {
 
 //Create dropdown menu with questions and pre-define answers.
 Blockly.Blocks['example_dropdown'] = {
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField('Ask me anything:')
-        .appendField(new Blockly.FieldDropdown([
-            ['What is the date today?', 'Date'],
-            ['What is the time now?', 'Time'],
-            ['How are you?', 'I am good'],
-            ['What is JavaScript?', 'JavaScript is a text-based programming language used both on the client-side and server-side that allows you to make web pages interactive.'],
-            ['What is your name?', 'Omkar Suresh Chaskar'],
-        ]), 'question');
-    this.setInputsInline(true);
+      .appendField('Ask me anything:')
+      .appendField(new Blockly.FieldDropdown([
+        ['What is the date today?', '1'],
+        ['What is the time now?', '2'],
+        ['How are you?', 'I am good'],
+        ['What is JavaScript?', 'JavaScript is a text-based programming language used both on the client-side and server-side that allows you to make web pages interactive.'],
+        ['What is your name?', 'Omkar Suresh Chaskar'],
+      ]), "question");
     this.setPreviousStatement(true, null);
     this.setColour(230);
     this.setTooltip("");
@@ -39,53 +39,42 @@ Blockly.Blocks['example_dropdown'] = {
   },
 };
 
-//Assign value of question field which value is null to make Bot block functional.
+//Assign value of question field which value pass from example_dropdown with statementToCode function with of argument and FILDNAME which store Field Value;
 Blockly.JavaScript["example_bot"] = function (block) {
-  var text_input = block.getFieldValue("question");
+  var text_input = Blockly.JavaScript.statementToCode(block, 'question');
   var code = `
-	var inputTextValue = "${text_input}";
-  `;
-  return code;
-};
-
-//On Run example_dropdown function get's selected questions answer and then print it over Bot Function
-Blockly.JavaScript["example_dropdown"] = function (block) {
-  var text_input = block.getFieldValue("question");
-  if(text_input == "Time"){
-    var today = new Date();
-    var time = today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
-    var new_text_input = time;
-    var code = `
-	var inputTextValue = "${new_text_input}";
-  `;
-  return code;
-  } else if(text_input == "Date"){
-    var today = new Date();
-    var time = today.getFullYear()+":"+today.getMonth()+":"+today.getDate();
-    var new_text_input = time;
-    var code = `
-	  var inputTextValue = "${new_text_input}";
-    `;
-    return code;
-  } else {
-    var code = `
     var inputTextValue = "${text_input}";
     `;
-    return code;
-  }
-  
+  return code;
 }
 
+//On Run example_dropdown function will return field value not gives back error
+Blockly.JavaScript["example_dropdown"] = function (block) {
+  return block.getFieldValue('question');
+}
 
 
 var workspace = Blockly.inject("blocklyDiv", {
   media: "assets/media/",
-  toolbox: document.getElementById("toolbox"),
+  toolbox: document.getElementById("toolbox")
 });
 
 function redrawUi() {
   if (typeof inputTextValue !== "undefined") {
-    $("#inputBox").text(inputTextValue);
+
+    //check for date and time option to givr current time and date
+    if (inputTextValue == 2) {
+      let today = new Date();
+      let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+      $("#inputBox").text(time);
+    } else if (inputTextValue == 1) {
+      let today = new Date();
+      let date = today.getFullYear() + ":" + today.getMonth() + ":" + today.getDate();
+      $("#inputBox").text(date);
+    } else {
+      $("#inputBox").text(inputTextValue);
+      console.log(typeof inputTextValue);
+    }
   } else {
     $("#inputBox").text("");
   }
@@ -105,6 +94,6 @@ function runcode() {
 function reset() {
   delete inputTextValue;
   redrawUi();
-  //To delete draged blocks from workspace 
+  //To devare draged blocks from workspace 
   workspace.clear();
 }
